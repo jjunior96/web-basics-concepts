f3df59387a781894a482d0658162b9b4fc489bbb
Junior <jjalves96@gmail.com>
2020-08-10 16:35:04 -0300

    finish challenge

--- Javascript/Challenges/1-Challenge/js/script.js
+++ Javascript/Challenges/1-Challenge/js/script.js
@@ -4,10 +4,62 @@
 
 // Crianças valem 0.5 pessoas
 
-let teste = document.getElementsByClassName("input-wrapper");
 
-console.log(teste);
+function calculaChurrasco() {
+  // Inputs 
+  let inputAdultos = document.getElementById("adultos");
+  let inputCriancas = document.getElementById("criancas");
+  let inputDuracao = document.getElementById("duracao");
 
-function retornaElemento() {
-  console.log("Testando");
+  // Onde sera inserido o resultado
+  let resultado = document.getElementById("result-wrapper");
+
+  // Carne
+  let quantidadeCarneAdultos = (inputAdultos.value * carnePorPessoa(inputDuracao.value));
+  let quantidadeCarneCriancas = inputCriancas.value * (carnePorPessoa(inputDuracao.value) / 2);
+  let quantidadeTotalCarne = quantidadeCarneAdultos + quantidadeCarneCriancas;
+
+  // Cerveja
+  let quantidadeTotalCerveja = inputAdultos.value * cervejaPorPessoa(inputDuracao);
+
+  // Refri/Agua 
+  let quantidadeTotalRefriAgua = (inputAdultos.value * aguaPorPessoa(inputDuracao)) + (inputCriancas.value * aguaPorPessoa(inputDuracao)/2);
+
+  // Insere no elemento com id #result-wrapper
+  resultado.innerHTML = `<p>${(quantidadeTotalCarne).toFixed(3)}Kg de carne</p>`;
+  resultado.innerHTML += `<p>${Math.ceil(quantidadeTotalCerveja / 0.350)} Latas 350ml de cerveja</p>`;
+  resultado.innerHTML += `<p>${Math.ceil(quantidadeTotalRefriAgua) / 2} Garrafas de 2l de água</p>`;
+
+  console.log(quantidadeTotalCerveja);
+
+}
+
+function carnePorPessoa(duracao) {
+  let carne = 0.4;
+
+  if (duracao >= 6 ) {
+    carne =  0.650;
+  } 
+  
+  return carne;
+}
+
+function cervejaPorPessoa(duracao) {
+  let cerveja = 1.2;
+
+  if (duracao >= 6 ) {
+    cerveja =  2;
+  } 
+  
+  return cerveja;
+}
+
+function aguaPorPessoa(duracao) {
+  let agua = 1;
+
+  if (duracao >= 6 ) {
+    agua =  1.5;
+  } 
+  
+  return agua;
 }
